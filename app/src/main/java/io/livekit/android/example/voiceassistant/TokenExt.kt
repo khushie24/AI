27582af7c6c244d8cda9df56b9ff1071d4fbb824
Khushie24 <ktanna780@gmail.com>
2026-06-22 20:10:40 +0530

    first commit

---
+++ app/src/main/java/io/livekit/android/example/voiceassistant/TokenExt.kt
@@ -0,0 +1,8 @@
+package io.livekit.android.example.voiceassistant
+
+// TODO: Add your Sandbox ID here
+
+const val sandboxID = "aisha-1suj7a"
+const val hardcodedUrl = ""
+const val hardcodedToken = ""
+const val agentName = "my-agent"
